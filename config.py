24814ca7fe79aee4bee77f8fe571c6d9a43f390e
hamzahsiraj <hamzah.siraj@gmail.com>
2020-03-05 10:27:31 -0600

    Access keys for AWS

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
-ACCESS_KEY = 'A###################'
+ACCESS_KEY = 'A####################'
 SECRET_KEY = 'X#################################'
 S3_LOCATION = 'https://{}.s3.aws.amazon.com/'.format('Bucket-Name')
