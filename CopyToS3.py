70a04892f2cbf9a5d7bc06af59997a96ac607d45
hamzahsiraj <hamzah.siraj@gmail.com>
2020-03-05 10:29:49 -0600

    Upload file from local computer to s3

--- CopyToS3.py
+++ CopyToS3.py
@@ -1,4 +1,4 @@
-import boto3   #install boto3 using pip
+import boto3   #install boto3 using pip.
 from config import ACCESS_KEY, SECRET_KEY    #config is the py file where the access keys are stored
 
 s3 = boto3.client("s3", aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
